6695b4300d82f76c3ae5900a86dcf74565598a0a
Mark Reid <mark@threewordslong.com>
2009-01-16 11:22:27 +1100

    Scripts

--- _scripts/import_comments.rb
+++ _scripts/import_comments.rb
@@ -1,3 +1,10 @@
-require 'rest-client'
+require 'rubygems'
+#require 'rest-client'
+require 'sequel'
 
 USER_KEY = ENV['DISQUS_KEY']
+
+DB = Sequel.mysql('inductio', :user=>'root', :password=>'', :host=>'localhost')
+DB["select * from wp_comments"].each |row| do
+  
+end
